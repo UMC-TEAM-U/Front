d47849dc5ff58033f4049aba9bec8d97612f8419
00kang <kdb070970@gmail.com>
2024-01-02 22:14:37 +0900

    #11 feat: 버튼 컴포넌트 구현

---
+++ src/component/common/Button.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { COLORS } from '../../styles/theme'
+
+function Button({ text, color }) {
+    return (
+        <ButtonStyle color={color}>
+            <span>{text}</span>
+        </ButtonStyle>
+    )
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+}
+
+export default Button
+
+const ButtonStyle = styled.button`
+    /* width: 69px; */
+    height: 33px;
+    border: none;
+    border-radius: 5px;
+    background-color: ${props =>
+        props.color === 'gray' ? COLORS.gray : COLORS.pink};
+    position: relative;
+
+    span {
+        color: ${COLORS.white};
+        font-family: PretendardVariable;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        padding: 22px 8px;
+    }
+`
