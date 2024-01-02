15d91855f4f096adb3cb0d5ac40b1e37b19a11fc
alreadynyeong <kjw87130@naver.com>
2024-01-02 20:16:17 +0900

    Rename: index.css ->GlobalStyles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
+import '../styles/fonts/Font.css'
 
 export const GlobalStyles = createGlobalStyle`
     body {
         margin: 0;
         width: 100vw;
         height: 100vh;
+        font-family: 'Pretendard';
     }
 `
